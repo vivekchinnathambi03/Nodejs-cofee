bb8e303c05f9ce8ac684c3a9271cb9e420438610
Vivek <vivekchinnathambi03@gmail.com>
2022-11-21 21:24:23 +0100

    cors issue

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const pricesFile = "./prices.json";
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:4200', 'https://www.google.com/']
-}));
 
-
+app.options('*', cors())
 app.get("/orders", function (req, res, next) {
     //res.send("Hello World");
     jsonfile.readFile(ordersFile, function (err, obj) {
@@ -19,14 +16,14 @@ app.get("/orders", function (req, res, next) {
         res.send(obj);
     });
 });
-
+app.options('*', cors())
 app.get("/payments", function (req,res) {
     jsonfile.readFile(paymentsFile, function (err, obj) {
         if (err) res.send(err);
         res.send(obj);
     });
 });
-
+app.options('*', cors())
 app.get("/prices", function (req,res) {
     jsonfile.readFile(pricesFile, function (err, obj) {
         if (err) res.send(err);
